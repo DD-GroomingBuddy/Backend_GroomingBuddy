ec5aa0bd7827d73cae64da1a41d5a0e7c0ff87e3
dsoldatic <soldaticdavid2@gmail.com>
2024-01-22 22:14:32 +0100

    novo

--- index.js
+++ index.js
@@ -7,3 +7,5 @@ const port = 3000; // port na kojem će web server slušati
 app.use(cors());
 
 app.listen(port, () => console.log(`\n\n[DONE] Backend se vrti na http://localhost:${port}/\n\n`));
+
+console.log ("David");
