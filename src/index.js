7f7f871c8a36dbc0e87241963685084577d18617
dsoldatic <soldaticdavid2@gmail.com>
2024-05-21 23:30:39 +0200

    index.js

--- src/index.js
+++ src/index.js
@@ -1,64 +1,41 @@
-import express from "express";
-import data from "./store.js";
-import { korisniciMethods } from "../handlers/korisnicihandler.js";
-import db from "./db.js";
+const express = require('express')
+const cookieSession = require("cookie-session");
 
 const app = express();
 const port = process.env.port || 3000;
+const db = require("./models");
+const Role = db.role;
+const cors = require("cors");
+
+
+var corsOptions = {
+  origin: "http://localhost:8080"
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
-
-app.get('/radnici', (req, res) => {
-    res.json(data.radnici);
+app.use(
+  cookieSession({
+    name: "GroomingBuddy",
+    keys: ["SECRET"], // should use as secret environment variable
+    httpOnly: true
+  })
+);
+// Database innit
+db.mongoose
+  .connect("mongodb+srv://test:test@clustertest.hehtbr4.mongodb.net/?retryWrites=true&w=majority&appName=ClusterTest", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    init();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
   });
-  
-  // Ruta za prikaz svih korisnika
-  app.get("/korisnici", korisniciMethods.getAllKorisnici);
-  
-  // Ruta za prikaz cjenik
-  app.get('/cjenik', (req, res) => {
-    res.json(data.cjenik);
-  });
-  
-  // Ruta za prikaz svih ocjena
-  app.get('/ocjena', (req, res) => {
-    res.json(data.ocjena);
-  });
-  
-  // Ruta za prikaz rezervacija
-  app.get('/rezervacije', (req, res) => {
-    res.json(data.rezervacija);
-  });
-  
-  // Dodavanje nove rezervacije
-  app.post('/rezervacije', (req, res) => {
-    const novaRezervacija = {
-      id: '301', // generiranje novog ID-a
-      datum: req.body.datum,
-      vrijeme: req.body.vrijeme,
-      korisnik: req.body.korisnik,
-      broj_tretman: req.body.broj_tretman,
-      ocjena: req.body.ocjena,
-    };
-  
-    data.rezervacija.push(novaRezervacija);
-    res.status(201).json(novaRezervacija);
-  });
-  
-  // Ruta za brisanje rezervacije po ID-u
-  app.delete('/rezervacije/:id', (req, res) => {
-    const { id } = req.params;
-    const index = data.rezervacija.findIndex((rezervacija) => rezervacija.id === id);
-  
-    if (index !== -1) {
-      const deletedReservation = data.rezervacija.splice(index, 1);
-      res.json(deletedReservation);
-    } else {
-      res.status(404).json({ message: 'Rezervacija nije pronađena.' });
-    }
-  });
-
-
 app.listen(port, () => {
   console.log(`Servis radi na portu ${port}`);
 });
