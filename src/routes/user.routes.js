e8dbc96765aca8d481d3100dfcd4c9ed5978bd08
dsoldatic <soldaticdavid2@gmail.com>
2024-06-02 14:08:02 +0200

    user.routes dodane

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,2 +1,29 @@
 const { authJwt } = require("../middleware");
 const controller = require("../controllers/user.controller");
+
+module.exports = function(app) {
+
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  app.post("/api/appointment/add", [authJwt.verifyToken],controller.appointmentAdd)
+
+  app.get("/api/appointment/get", [authJwt.verifyToken], controller.getAppointments);
+
+  app.get("/api/appointment/getAll", [authJwt.verifyToken , authJwt.isAdmin], controller.getAllAppointments)
+
+  app.get("/api/test/all", controller.allAccess);
+
+  app.get("/api/test/user", [authJwt.verifyToken], controller.userBoard);
+
+  app.get(
+    "/api/test/admin",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    controller.adminBoard
+  );
+};
