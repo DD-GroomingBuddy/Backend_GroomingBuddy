d2f5ab39023fbd5a0eacbb2f07f4f6b86274e3f4
dsoldatic <soldaticdavid2@gmail.com>
2024-06-09 22:22:06 +0200

    pokusaj popravka

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,46 +1,25 @@
 const { verifySignUp, authJwt } = require("../middleware");
 const controller = require("../controllers/auth.controller");
+const cors = require('cors');
 
 module.exports = function (app) {
-  app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Headers", "x-access-token ,Origin, Content-Type, Accept");
-    next();
-  });
+  // Global CORS settings if applicable
+  app.use(cors());
 
   app.post(
     "/api/auth/signup",
+    cors(), // Apply CORS per-route basis if specific origins needed
     [
       verifySignUp.checkDuplicateUsernameOrEmail,
-      verifySignUp.checkRolesExisted,
+      verifySignUp.checkRolesExisted
     ],
     controller.signup
   );
 
-  app.post("/api/auth/signin", controller.signin);
-
-  app.post("/api/auth/signout", controller.signout);
-
-  app.post(
-    "/api/auth/appointment/add",
-    [authJwt.verifyToken],
-    controller.appointmentAdd
-  );
-
-  app.get(
-    "/api/auth/appointment/get",
-    [authJwt.verifyToken],
-    controller.getAppointments
-  );
-
-  app.get(
-    "/api/auth/appointment/getAll",
-    [authJwt.verifyToken, authJwt.isAdmin],
-    controller.getAllAppointments
-  );
-
-  app.delete(
-    "/api/auth/appointment/delete",
-    [authJwt.verifyToken, authJwt.isAdmin],
-    controller.appointmentDelete
-  );
+  app.post("/api/auth/signin", cors(), controller.signin);
+  app.post("/api/auth/signout", cors(), controller.signout);
+  app.post("/api/auth/appointment/add", [authJwt.verifyToken], controller.appointmentAdd);
+  app.get("/api/auth/appointment/get", [authJwt.verifyToken], controller.getAppointments);
+  app.get("/api/auth/appointment/getAll", [authJwt.verifyToken, authJwt.isAdmin], controller.getAllAppointments);
+  app.delete("/api/auth/appointment/delete", [authJwt.verifyToken, authJwt.isAdmin], controller.appointmentDelete);
 };
